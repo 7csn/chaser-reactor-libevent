020e3d10a64a18a97a62f5c6fc882addbecf0a41
7csn <1479672753@qq.com>
2021-06-05 15:28:31 +0800

    .

--- src/Libevent.php
+++ src/Libevent.php
@@ -2,8 +2,6 @@
 
 namespace chaser\reactor;
 
-use Throwable;
-
 /**
  * 基于 libevent 扩展的事件反应类
  *
@@ -29,45 +27,69 @@ class Libevent extends Reactor
     /**
      * @inheritDoc
      */
-    protected function addReadData(int $intFd, $fd, callable $callback)
+    public function loop(): void
     {
-        return $this->makeEvent($fd, EV_READ | EV_PERSIST, $callback, $fd);
+        event_base_loop($this->eventBase);
     }
 
     /**
      * @inheritDoc
      */
-    protected function addWriteData(int $intFd, $fd, callable $callback)
+    public function destroy(): void
     {
-        return $this->makeEvent($fd, EV_WRITE | EV_PERSIST, $callback, $fd);
+        event_base_loopexit($this->eventBase);
     }
 
     /**
      * @inheritDoc
+     *
+     * @return resource|false
      */
-    protected function addSignalData(int $key, int $signal, callable $callback)
+    protected function makeReadData(int $intFd, $fd, callable $callback)
     {
-        return $this->makeEvent($signal, EV_SIGNAL | EV_PERSIST, $callback, $signal);
+        return $this->makeEvent($fd, EV_READ | EV_PERSIST, $callback);
     }
 
     /**
      * @inheritDoc
+     *
+     * @return resource|false
      */
-    protected function addIntervalData(int $timerId, int $seconds, callable $callback)
+    protected function makeWriteData(int $intFd, $fd, callable $callback)
+    {
+        return $this->makeEvent($fd, EV_WRITE | EV_PERSIST, $callback);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @return resource|false
+     */
+    protected function makeSignalData(int $signal, callable $callback)
+    {
+        return $this->makeEvent($signal, EV_SIGNAL | EV_PERSIST, $callback);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @return array|false
+     */
+    protected function makeIntervalData(int $timerId, int $seconds, callable $callback): array|false
     {
         $interval = $seconds * 1000000;
-
-        $event = $this->makeEvent(0, EV_TIMEOUT, fn() => $this->timerCallback($timerId, self::EV_INTERVAL), $timerId, $interval);
-
+        $event = $this->makeEvent(0, EV_TIMEOUT, fn() => $this->timerCallback($timerId, self::EV_INTERVAL), $interval);
         return $event ? [$event, $callback, $interval] : false;
     }
 
     /**
      * @inheritDoc
+     *
+     * @return array|false
      */
-    protected function addTimeoutData(int $timerId, int $seconds, callable $callback)
+    protected function makeTimeoutData(int $timerId, int $seconds, callable $callback): array|false
     {
-        $event = $this->makeEvent(0, EV_TIMEOUT, fn() => $this->timerCallback($timerId, self::EV_TIMEOUT), $timerId, $seconds * 1000000);
+        $event = $this->makeEvent(0, EV_TIMEOUT, fn() => $this->timerCallback($timerId, self::EV_TIMEOUT), $seconds * 1000000);
         return $event ? [$event, $callback, 0] : false;
     }
 
@@ -77,16 +99,15 @@ class Libevent extends Reactor
      * @param mixed $fd
      * @param int $flags
      * @param callable $callback
-     * @param null $arg
      * @param int $timeout
      * @return false|resource
      */
-    protected function makeEvent($fd, int $flags, callable $callback, $arg = null, int $timeout = -1)
+    protected function makeEvent(mixed $fd, int $flags, callable $callback, int $timeout = -1)
     {
         $event = event_new();
 
         // 准备事件
-        if (!event_set($event, $fd, $flags, $callback, $arg)) {
+        if (!event_set($event, $fd, $flags, $callback)) {
             return false;
         }
 
@@ -106,7 +127,7 @@ class Libevent extends Reactor
     /**
      * @inheritDoc
      */
-    protected function delCallback(int $flag, int $key): bool
+    protected function delDataModel(int $flag, int $key): bool
     {
         switch ($flag) {
             case self::EV_READ:
@@ -126,39 +147,15 @@ class Libevent extends Reactor
      * @param int $timerId
      * @param int $flag
      */
-    public function timerCallback(int $timerId, int $flag)
+    private function timerCallback(int $timerId, int $flag): void
     {
         if (isset($this->events[$flag][$timerId])) {
 
             [$event, $callback, $timeout] = $this->events[$flag][$timerId];
 
-            if ($flag === self::EV_TIMEOUT) {
-                $this->delTimeout($timerId);
-            } else {
-                event_add($event, $timeout);
-            }
+            $flag === self::EV_TIMEOUT ? $this->delTimeout($timerId) : event_add($event, $timeout);
 
-            try {
-                $callback($timerId);
-            } catch (Throwable $e) {
-                exit(250);
-            }
+            $callback($timerId);
         }
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function loop(): void
-    {
-        event_base_loop($this->eventBase);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function destroy(): void
-    {
-        event_base_loopbreak($this->eventBase);
     }
 }
